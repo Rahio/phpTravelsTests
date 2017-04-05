bda10d37bcb9dfc15ba7b3cae81598cabf2a6983
Rahi <matheo0309@gmail.com>
2017-04-06 01:15:00 +0200

    Login tests for admin and supplier module added

---
+++ src/test/java/com/selenium/test/testng/tests/adminModule/LoginTests.java
@@ -0,0 +1,7 @@
+package com.selenium.test.testng.tests.adminModule;
+
+/**
+ * Created by Rahi on 2017-04-06.
+ */
+public class LoginTests {
+}
---
+++ src/test/java/com/selenium/test/testng/tests/supplierModule/LoginTests.java
@@ -0,0 +1,7 @@
+package com.selenium.test.testng.tests.supplierModule;
+
+/**
+ * Created by Rahi on 2017-04-06.
+ */
+public class LoginTests {
+}
